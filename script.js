8b69e3cb6be52af3ee25885140df2640905b2f15
Diana Nesterenko <78849668+diananesterenko@users.noreply.github.com>
2022-07-10 21:19:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function timeConverter(UNIX_timestamp) {
 }
 function getForecast(coordinates) {
   let apiKey = "48c14604b04302ce48bd992c15188679";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function showtemp(response) {
